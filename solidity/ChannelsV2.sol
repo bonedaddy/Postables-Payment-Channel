0f424b261f38b2dcb4cb5b38672df84888ac7976
postables <postables@none.com>
2018-02-06 00:46:21 -0800

    Added vendor proof submission, and two-in-one channel opening

--- solidity/ChannelsV2.sol
+++ solidity/ChannelsV2.sol
@@ -55,6 +55,11 @@ contract PaymentChannels is Administration {
 
 	function () public payable {}
 
+
+	/**
+		Used to open a channeltwo-in-one vendor verification
+		Useful in situations where the vendor and purchaser don't know each other.
+	*/
 	function openChannel(
 		address _vendor,
 		uint256 _channelValueInWei,
@@ -69,6 +74,7 @@ contract PaymentChannels is Administration {
 		bytes32 channelId = keccak256(msg.sender, _vendor, _channelValueInWei, currentDate);
 		// make sure the channel id doens't already exit
 		require(!channelIds[channelId]);
+		channelIds[channelId] = true;
 		uint256 autoClosureDate = (now + ((_durationInDays * 1 days) * 2));
 		channels[channelId].purchaser = msg.sender;
 		channels[channelId].vendor = _vendor;
@@ -81,6 +87,68 @@ contract PaymentChannels is Administration {
 		return true;
 	}
 
+	/**
+		Used to open a channel, and submit vendor proof all at once
+		Useful in situations where there is some level of turst between vendor and purchaser
+		NOT TESTED
+	*/
+	function openChannel(
+		address _vendor,
+		uint256 _channelValueInWei,
+		uint256 _durationInDays,
+		bytes32 _h,
+		uint8   _v,
+		bytes32 _r,
+		bytes32 _s)
+		public
+		payable
+		returns (bool)
+	{
+		require(msg.value == _channelValueInWei);
+		uint256 currentDate = now;
+		// channel hash = keccak256(purchaser, vendor, channel value, date of open)
+		bytes32 channelId = keccak256(msg.sender, _vendor, _channelValueInWei, currentDate);
+		// make sure the channel id doens't already exit
+		require(!channelIds[channelId]);
+		channelIds[channelId] = true;
+		uint256 autoClosureDate = (now + ((_durationInDays * 1 days) * 2));
+		channels[channelId].purchaser = msg.sender;
+		channels[channelId].vendor = _vendor;
+		channels[channelId].value = _channelValueInWei;
+		channels[channelId].expirationDate = autoClosureDate;
+		channels[channelId].openDate = currentDate;
+		channels[channelId].channelId = channelId;
+		ChannelOpened(msg.sender, _vendor, channelId);
+		require(submitVendorProof(_h, _v, _r, _s, channelId));
+		return true;
+	}
+
+
+	/**
+		Used to submit vendor proof
+		NOT TESTED
+	*/
+	function submitVendorProof(
+		bytes32 _h,
+		uint8   _v,
+		bytes32 _r,
+		bytes32 _s,
+		bytes32 _channelId)
+		public
+		returns (bool)
+	{
+		require(channelIds[_channelId]);
+		require(channels[_channelId].state != ChannelStates.closed &&
+			    channels[_channelId].state != ChannelStates.expired &&
+			    channels[channelId].state != ChannelStates.accepted);
+		channels[channelId].state = ChannelStates.accepted;
+		address signer = ecrecover(_h, _v, _r, _s);
+		require(signer == channels[_channelId].vendor);
+		channels[_channelId].proofSubmitted[signer] = true;
+		VendorProofSubmitted(_channelId, signer);
+		return true;
+	}
+
 	function withdrawEth()
 		public
 		onlyAdmin
@@ -89,6 +157,72 @@ contract PaymentChannels is Administration {
 		require(dev);
 		msg.sender.transfer(this.balance);
 		return true;
+	}
+
+	/**GETTERS*/
+
+	function getChannelPurchaser(
+		bytes32 _channelId)
+		public
+		view
+		returns (address)
+	{
+		return channels[_channelId].purchaser;
+	}
+
+	function getChannelVendor(
+		bytes32 _channelId)
+		public
+		view
+		returns (address)
+	{
+		return channels[_channelId].vendor;
+	}
+
+	function getChannelValue(
+		bytes32 _channelId)
+		public
+		view
+		returns (uint256)
+	{
+		return channels[_channelId].value;
+	}
+
+	function getExpirationDate(
+		bytes32 _channelId)
+		public
+		view
+		returns (uint256)
+	{
+		return channels[_channelId].expirationDate;
+	}
+
+	function getOpenDate(
+		bytes32 _channelId)
+		public
+		view
+		returns (uint256)
+	{
+		return channels[_channelId].openDate;
+	}
+
+	function getChannelState(
+		bytes32 _channelId)
+		public
+		view
+		returns (uint256)
+	{
+		return channels[_channelId].state;
+	}
+
+	function checkIfProofSubmitted(
+		bytes32 _channelId,
+		address _addr)
+		public
+		view
+		returns (bool)
+	{
+		return channels[_channelId].proofSubmitted[_addr];
 	}
 
 }
