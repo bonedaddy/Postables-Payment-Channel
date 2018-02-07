b93f5b650ecaea66f84b3d6873a84d6dc9400e05
postables <postables@none.com>
2018-02-06 23:25:29 -0800

    Added `signer.py` to be used to generate the associated signature for the payment channel hubs

---
+++ python/signer.py
@@ -0,0 +1,21 @@
+from web3 import Web3
+import json
+import getpass
+
+w3 = Web3()
+
+keyFilePath = input('Enter the absolute path to your key file:\t')
+keyFilePassword = getpass.getpass('Enter the password to decrypt your key file:\t')
+with open(keyFilePath, 'r') as fh:
+	private_key = w3.eth.account.decrypt(json.load(fh), keyFilePassword)
+
+
+messageTextToSign = input('What is the message you wish to sign:\t')
+signature = w3.eth.account.sign(message_text=messageTextToSign, private_key=private_key)
+h = Web3.toHex(signature.messageHash)
+v = signature.v
+# zero-pad with 32 bytes, issue #617 in web3py
+r = Web3.toHex(Web3.toBytes(signature.r).rjust(32, b'\0'))
+s = Web3.toHex(Web3.toBytes(signature.s).rjust(32, b'\0'))
+
+print("h\t\t",h,"\nv\t\t",v,"\nr\t\t",r,"\ns\t\t",s,"\n")
